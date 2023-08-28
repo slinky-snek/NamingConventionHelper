935d7bbc6c119ec312ae372fe71561bbe1bc93b8
slinky-snek <contactaaronbarnett@gmail.com>
2023-08-28 17:20:21 -0400

    Initial commit. Renaming currently loops because rename code runs when asset is added to registry, added to registry event calls rename which causes added to registry event. Forever loop. Currently dealing with access violation as well. Also, DoesPrefixExistInName apparently doesn't work correctly. Asset prefix never matches current prefix which is impossible.

---
+++ Plugins/NamingConventionHelper/Source/NamingConventionHelper/Private/AutoNamePrefixer.cpp
@@ -0,0 +1,68 @@
+﻿#include "AutoNamePrefixer.h"
+#include "Editor.h"
+#include "AssetRegistry/AssetRegistryModule.h"
+#include "Factories/Factory.h"
+#include "EditorScriptingUtilities/Public/EditorAssetLibrary.h"
+
+void UAutoNamePrefixer::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+	const FAssetRegistryModule& AssetRegistryModule = FModuleManager::LoadModuleChecked<FAssetRegistryModule>(TEXT("AssetRegistry"));
+	AssetRegistryModule.Get().OnAssetAdded().AddUObject(this, &UAutoNamePrefixer::OnAssetAdded);
+	FEditorDelegates::OnNewAssetCreated.AddUObject(this, &UAutoNamePrefixer::OnAssetCreated);
+}
+
+void UAutoNamePrefixer::Deinitialize()
+{
+	Super::Deinitialize();
+}
+
+void UAutoNamePrefixer::OnAssetAdded(const FAssetData& AssetData)
+{
+	if(IsEditorFullyLoaded)
+		RenameAsset(AssetData);
+}
+
+void UAutoNamePrefixer::OnAssetCreated(UFactory* Factory)
+{
+	if(!IsEditorFullyLoaded)
+		IsEditorFullyLoaded = true;
+}
+
+void UAutoNamePrefixer::RenameAsset(const FAssetData& AssetData)
+{
+	if(AssetData.GetAsset() && AssetData.AssetClassPath.GetAssetName() != FName("BlueprintGeneratedClass"))
+	{
+		const FString PackageName = AssetData.PackagePath.ToString();
+		const FString AssetName = AssetData.AssetName.ToString();
+		const FString OldAssetPath = FString::Printf(TEXT("%s/%s"), *PackageName, *AssetName);
+		const FString NewName = GetAssetPrefix(AssetData.AssetClassPath).Append(AssetName);
+		const FString NewAssetPath = FString::Printf(TEXT("%s/%s"), *PackageName, *NewName);
+		if(!DoesPrefixExistInName(AssetData))
+			if(UEditorAssetLibrary::RenameAsset(OldAssetPath, NewAssetPath)) //loops forever because renaming causes the OnAssetAdded event to fire
+				GEngine->AddOnScreenDebugMessage(INDEX_NONE, 10, FColor::Red, TEXT("Successfully renamed asset"));
+			else
+				GEngine->AddOnScreenDebugMessage(INDEX_NONE, 10, FColor::Red, TEXT("Failed to rename asset"));
+	}
+}
+
+FString UAutoNamePrefixer::GetAssetPrefix(const FTopLevelAssetPath ClassPath)
+{
+	FString Prefix = "BP_";
+	return Prefix;
+}
+
+bool UAutoNamePrefixer::DoesPrefixExistInName(const FAssetData& AssetData)
+{
+	TArray<FString> AssetNameParts;
+	const FString AssetName = AssetData.AssetName.ToString();
+	const FString Delimiter = "_";
+	const FString Prefix = GetAssetPrefix(AssetData.AssetClassPath);
+	AssetName.ParseIntoArray(AssetNameParts, *Delimiter);
+	FString CurrentPrefix;
+	if(AssetNameParts.Num() > 0)
+		CurrentPrefix = AssetNameParts[0].Append("_");
+	if(Prefix == CurrentPrefix) //why does this always return false???
+		return true;
+	return false;
+}
---
+++ Plugins/NamingConventionHelper/Source/NamingConventionHelper/Public/AutoNamePrefixer.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "EditorSubsystem.h"
+#include "AutoNamePrefixer.generated.h"
+
+class UFactory;
+
+UCLASS()
+class NAMINGCONVENTIONHELPER_API UAutoNamePrefixer : public UEditorSubsystem
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	virtual void Deinitialize() override;
+
+	void OnAssetCreated(UFactory* Factory);
+	void OnAssetAdded(const FAssetData& AssetData);
+	static void RenameAsset(const FAssetData& AssetData);
+	static FString GetAssetPrefix(const FTopLevelAssetPath ClassPath);
+	static bool DoesPrefixExistInName(const FAssetData& AssetData);
+
+private:
+	inline static bool IsEditorFullyLoaded = false;
+	inline static bool RenamingInProgress = false;
+};
